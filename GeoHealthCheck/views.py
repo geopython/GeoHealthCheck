4d54a525b1d4ac764c17d6890ce65fe12594a0d2
Just van den Broecke <just@justobjects.nl>
2016-09-19 13:16:10 +0200

    fix #58 key error

--- GeoHealthCheck/views.py
+++ GeoHealthCheck/views.py
@@ -62,13 +62,8 @@ def list_resources(resource_type=None, query=None):
         response['resources'] = models.Resource.query.filter(
             field.ilike(term)).all()
 
-<<<<<<< HEAD
     if 'resources' not in response:
         # No query nor resource_type provided: fetch all resources
-=======
-    # No query nor resource_type provided: fetch all resources
-    if response['resources'] is None:
->>>>>>> upstream/master
         response['resources'] = models.Resource.query.all()
 
     response['total'] = len(response['resources'])
