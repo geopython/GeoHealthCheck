25e2ad285d8a3aaeb708bce734df6bc08bded2f1
Just van den Broecke <just@justobjects.nl>
2017-06-26 13:14:32 +0200

    #125 first stab for WFS GetFeature Probe using Caps metadata

--- GeoHealthCheck/plugins/probe/wfs.py
+++ GeoHealthCheck/plugins/probe/wfs.py
@@ -1,4 +1,5 @@
 from GeoHealthCheck.probe import Probe
+from owslib.wfs import WebFeatureService
 
 
 class WfsGetFeatureBbox(Probe):
@@ -22,13 +23,12 @@ service="WFS"
 version="1.1.0"
 outputFormat="GML2"
 xsi:schemaLocation="http://www.opengis.net/wfs
-http://schemas.opengis.net/wfs/1.1.0/wfs.xsd"
+http://schemas.opengis.net/wfs/1.1.0/wfs.xsd" 
 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
   <wfs:Query typeName="{type_name}" srsName="{srs}"
         xmlns:{type_ns_prefix}="{type_ns_uri}">
     <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
       <ogc:BBOX>
-        <ogc:PropertyName>{geom_property_name}</ogc:PropertyName>
         <gml:Envelope xmlns:gml="http://www.opengis.net/gml" srsName="{srs}">
           <gml:lowerCorner>{bbox[0]} {bbox[1]}</gml:lowerCorner>
           <gml:upperCorner>{bbox[2]} {bbox[3]}</gml:upperCorner>
@@ -47,7 +47,8 @@ xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
             'type': 'string',
             'description': 'The WFS FeatureType name',
             'default': None,
-            'required': True
+            'required': True,
+            'range': None
         },
         'type_ns_prefix': {
             'type': 'string',
@@ -66,8 +67,9 @@ xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
         'geom_property_name': {
             'type': 'string',
             'description': 'Name of the geometry property within FeatureType',
-            'default': 'the_geom',
+            'default': None,
             'required': True,
+            'value': 'Not Required',
             'range': None
         },
         'srs': {
@@ -112,3 +114,52 @@ xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
     Optionally override Check PARAM_DEFS using set_params
     e.g. with specific `value` or even `name`.
     """
+
+    # Overridden: expand param-ranges from WMS metadata
+    def expand_params(self, resource):
+
+        # Use WMS Capabilities doc to get metadata for
+        # PARAM_DEFS ranges/defaults
+        try:
+            wfs = WebFeatureService(resource.url, version="1.1.0")
+            feature_types = wfs.contents
+            feature_type_names = list(feature_types.keys())
+            ft_namespaces = list(set([name.split(':')[0] if ':' in name else None for name in feature_type_names]))
+            ft_namespaces = filter(None, ft_namespaces)
+            if len(ft_namespaces) > 0:
+                nsmap = wfs._capabilities.nsmap
+            else:
+                ft_namespaces = ['notapplicable']
+                nsmap = {ft_namespaces[0]: 'http://not.appli.cable/'}
+                self.PARAM_DEFS['type_ns_prefix']['value'] = ft_namespaces[0]
+                self.PARAM_DEFS['type_ns_uri']['value'] = nsmap[ft_namespaces[0]]
+
+            # Layers to select
+            self.PARAM_DEFS['type_name']['range'] = feature_type_names
+            self.PARAM_DEFS['type_ns_prefix']['range'] = ft_namespaces
+            self.PARAM_DEFS['type_ns_uri']['range'] = [nsmap[ns] for ns in ft_namespaces]
+
+            # Image Format
+            # for oper in wfs.operations:
+            #     if oper.name == 'GetFeature':
+            #         self.PARAM_DEFS['format']['range'] = \
+            #             oper.formatOptions
+            #         break
+
+            # Take first feature_type to determine generic attrs
+            feature_type_name, feature_type_entry = feature_types.popitem()
+
+            # SRS
+            crs_list = feature_type_entry.crsOptions
+            srs_range = ['EPSG:%s' % crs.code for crs in crs_list]
+            self.PARAM_DEFS['srs']['range'] = srs_range
+
+            # bbox list: 0-3 is bbox, 4 is SRS
+            bbox = feature_type_entry.boundingBoxWGS84
+            self.PARAM_DEFS['srs']['default'] = srs_range[0]
+            self.PARAM_DEFS['bbox']['default'] = \
+                ['{:.2f}'.format(x) for x in bbox]
+
+            # self.PARAM_DEFS['exceptions']['range'] = wfs.exceptions
+        except Exception as err:
+            raise err
