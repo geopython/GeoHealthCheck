f3209109db78f051a8669da0c7592c407fc8b7b9
Rob van Loon <rob.v.loon@gmail.com>
2019-09-13 19:56:41 +0200

    Remove UTF-8 indication
    Python 3 is UTF-8 by default. Als update on shebang.

--- GeoHealthCheck/app.py
+++ GeoHealthCheck/app.py
@@ -1,4 +1,3 @@
-# coding=utf-8
 # =================================================================
 #
 # Authors: Tom Kralidis <tomkralidis@gmail.com>
--- GeoHealthCheck/plugin.py
+++ GeoHealthCheck/plugin.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from factory import Factory
 import logging
 import inspect
