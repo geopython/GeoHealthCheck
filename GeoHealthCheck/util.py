e47a127e629443a086163ca352ece665192a4fe5
Rob van Loon <rob.v.loon@gmail.com>
2019-10-18 18:09:14 +0200

    Fix auth encoding/decoding
    From code review by @justb4:
    6 - Auth info Decoding/Encoding Failure with Postgres
    
    Only occurs when using Postgres, not with SQLite. Background: GHC
    encodes/encrypts a generic auth dict info structure via JSON string to be stored
    as textfield in DB. It decodes/decrypts when reading. This way we can support
    multiple auth types with a single auth column in resource table. So this is
    another encoding than in 4 for HTTP auth headers, but think similar problem.
    We also need to deal with existing PG DBs that have auth columns already present
    in resource table.
    
    -  install psycopg2 : pip install psycopg2
    -  used existing PG DB, only changed in config_site.py: SQLALCHEMY_DATABASE_URI
       = 'postgresql://name:passw@localhost:5432/ghc', but may create new
    -  add URL Resource with Basic Auth (even does not have to have basic auth)
    -  in Edit add Basic Auth Username and Password
    -  click Save
    
    Fixed by ensuring encode/decode takes `string` as input and gives `string` as
    output. These methodes now use static typing. The origin of the problem was the
    difference of how SQLite and Postgres store the encoded string (either as text
    or in some binary form). This solution makes the encoded authentication a
    string object and *not* bytes.

--- GeoHealthCheck/util.py
+++ GeoHealthCheck/util.py
@@ -32,7 +32,6 @@ import json
 import logging
 import os
 import smtplib
-import six
 import base64
 from urllib.request import urlopen
 from urllib.parse import urlparse
@@ -228,22 +227,24 @@ def read(filename, encoding='utf-8'):
 
 
 # https://gist.github.com/gowhari/fea9c559f08a310e5cfd62978bc86a1a
-def encode(key, string):
+# with alterations to get return type of string, not bytes
+def encode(key: str, string: str) -> str:
     encoded_chars = []
     for i in range(len(string)):
         key_c = key[i % len(key)]
         encoded_c = chr(ord(string[i]) + ord(key_c) % 256)
         encoded_chars.append(encoded_c)
     encoded_string = ''.join(encoded_chars)
-    encoded_string = encoded_string.encode('latin') \
-        if six.PY3 else encoded_string
-    return base64.urlsafe_b64encode(encoded_string).rstrip(b'=')
+    encoded_string = encoded_string.encode('latin')
+    encoded_b64 = base64.urlsafe_b64encode(encoded_string).rstrip(b'=')
+    return encoded_b64.decode()
 
 
 # https://gist.github.com/gowhari/fea9c559f08a310e5cfd62978bc86a1a
-def decode(key, string):
-    string = base64.urlsafe_b64decode(string + b'===')
-    string = string.decode('latin') if six.PY3 else string
+# with alterations: expecting string as input (not bytes)
+def decode(key: str, string: str) -> str:
+    string = base64.urlsafe_b64decode(string.encode() + b'===')
+    string = string.decode('latin')
     encoded_chars = []
     for i in range(len(string)):
         key_c = key[i % len(key)]
