4a6707241a25dbeb006983800eb2c04000281dc6
Rob van Loon <borrob@me.com>
2020-01-26 13:32:46 +0100

    Add a fixed location geocoder
    This is usefull for running GHC in a private environment where geocoding the server
    does not make any sense.

---
+++ GeoHealthCheck/plugins/geocode/fixedlocation.py
@@ -0,0 +1,50 @@
+from GeoHealthCheck.geocoder import Geocoder
+
+
+class FixedLocation(Geocoder):
+    """
+    Spoof getting  a geolocation for a server by provinding a fixed lat, lon
+    result. The lat, lon can be specified in the initialisation parameters.
+    When omitted: default to 0, 0.
+    """
+
+    NAME = 'Fixed geolocation'
+
+    DESCRIPTION = 'Geolocater service returning a fixed position (so ' \
+                  'actually no real geolocation).'
+
+    LATITUDE = 0
+    """
+    Parameter with the default latitude position. This is overruled when the
+    latitude option is provided in the init step.
+    """
+    
+    LONGITUDE = 0
+    """
+    Parameter with the default longitude position. This is overruled when the
+    longitude option is provided in the init step.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self._lat = self.LATITUDE
+        self._lon = self.LONGITUDE
+
+    def init(self, geocode_vars={}):
+        """
+        Initialise the geocoder service with an optional dictionary.
+        
+        When the dictionary contains the element `lat` and/or `lon`, then these
+        values are used to position the server.
+        """
+        super().init(geocoder_vars)
+        self._lat = geocode_vars.get('lat', self.LATITUDE)
+        self._lon = geocode_vars.get('lon', self.LONGITUDE)
+
+    def locate(self, _=None):
+        """
+        Perform a geocoding to locate a server. In this case it will render a
+        fixed position, so provinding the adress of the server is optional.
+        """
+        return (self._lat, self._lon)
+
