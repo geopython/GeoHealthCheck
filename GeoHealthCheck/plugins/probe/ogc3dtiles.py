66f55c8ce817479332b0265210f96a014fd375ce
Jochem 't Hart <75669163+jochemthart1@users.noreply.github.com>
2021-11-08 12:54:38 +0100

    add description, remove print and log statements

--- GeoHealthCheck/plugins/probe/ogc3dtiles.py
+++ GeoHealthCheck/plugins/probe/ogc3dtiles.py
@@ -8,7 +8,7 @@ class OGC3DTiles(Probe):
     """
 
     NAME = 'GET Tileset.json and tile data'
-    DESCRIPTION = 'OGC3DTiles'
+    DESCRIPTION = 'Request tileset.json, recursively find and request tile data url'
     RESOURCE_TYPE = 'OGC:3DTiles'
     REQUEST_METHOD = 'GET'
 
@@ -33,9 +33,7 @@ class OGC3DTiles(Probe):
             tile_url = url_base + '/tileset.json'
             self.log('Requesting: %s url=%s' % (self.REQUEST_METHOD, tile_url))
             self.response = Probe.perform_get_request(self, tile_url)
-            self.log('test1')
             self.run_checks()
-            self.log('test2')
         except requests.exceptions.RequestException as e:
             msg = "Request Err: Error requesting tileset.json %s %s" \
                 % (e.__class__.__name__, str(e))
@@ -47,7 +45,6 @@ class OGC3DTiles(Probe):
         try:
             tile_root = self.response.json()['root']
             data_uri = self.get_3d_tileset_content_uri(tile_root)
-            print('DATA URI', data_uri)
             data_url = url_base + '/' + data_uri
             self.log('Requesting: %s url=%s' % (self.REQUEST_METHOD, data_url))
             self.response = Probe.perform_get_request(self, data_url)
