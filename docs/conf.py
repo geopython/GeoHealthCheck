39138c74645d9751fed9b1535681e6ec45147eb9
Just van den Broecke <just@justobjects.nl>
2023-11-19 14:09:33 +0100

    #467 updates for ReadTheDocs generation - auto API

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,9 @@ sys.path.insert(0, os.path.abspath('../GeoHealthCheck'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.viewcode', "sphinx_rtd_theme"]
+extensions = ['autoapi.extension', 'sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.viewcode', 'sphinx_rtd_theme']
+
+autoapi_dirs = ['../GeoHealthCheck/']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
