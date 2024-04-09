743252d2b9988221977b458137abd950661a498a
Patrick Noll <patrick.noll@vermkv.rlp.de>
2024-04-09 17:30:02 +0200

    Extending the WFS-Probe request to handle the count parameter. max_count added to the PARAM_DEFS with a default of 1000.

--- GeoHealthCheck/plugins/probe/wfs.py
+++ GeoHealthCheck/plugins/probe/wfs.py
@@ -27,7 +27,7 @@ outputFormat="text/xml; subtype=gml/3.1.1"
 xsi:schemaLocation="http://www.opengis.net/wfs
 http://schemas.opengis.net/wfs/1.1.0/wfs.xsd"
 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-  <wfs:Query typeName="{type_name}" srsName="{srs}"
+  <wfs:Query typeName="{type_name}" srsName="{srs}" count="{max_count}"
         xmlns:{type_ns_prefix}="{type_ns_uri}">
     <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
       <ogc:BBOX>
@@ -74,6 +74,13 @@ xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
             'type': 'string',
             'description': 'The SRS as EPSG: code',
             'default': 'EPSG:4326',
+            'required': True,
+            'range': None
+        },
+        'max_count': {
+            'type': 'string',
+            'description': 'Maximum amount of features to select',
+            'default': '1000',
             'required': True,
             'range': None
         },
