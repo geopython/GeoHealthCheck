f91cba144406c5f6ac78e6d1c407cf663bb6777a
Patrick Noll <patrick.noll@vermkv.rlp.de>
2024-04-11 13:13:44 +0200

    Fix a typo. Added max_count to fixtures.json and resources.json

--- GeoHealthCheck/plugins/probe/wfs.py
+++ GeoHealthCheck/plugins/probe/wfs.py
@@ -27,7 +27,7 @@ outputFormat="text/xml; subtype=gml/3.1.1"
 xsi:schemaLocation="http://www.opengis.net/wfs
 http://schemas.opengis.net/wfs/1.1.0/wfs.xsd"
 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-  <wfs:Query typeName="{type_name}" srsName="{srs}" maxfeatures="{max_count}"
+  <wfs:Query typeName="{type_name}" srsName="{srs}" maxFeatures="{max_count}"
         xmlns:{type_ns_prefix}="{type_ns_uri}">
     <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
       <ogc:BBOX>
@@ -81,7 +81,7 @@ xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
             'type': 'string',
             'description': 'Maximum amount of features to select',
             'default': '5',
-            'required': False,
+            'required': True,
             'range': None
         },
         'bbox': {
